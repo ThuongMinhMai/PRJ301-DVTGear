00ad0de1f9fa524f482f63e61b65951892768b38
TruongDuyTven <duyvntse170126@fpt.edu.vn>
2023-06-12 06:56:02 +0700

    add tab in order

--- UI_TOLStore/Javascript/order.js
+++ UI_TOLStore/Javascript/order.js
@@ -1,3 +1,26 @@
+
+// Tab
+
+// Get all the tab elements
+const tabs = document.querySelectorAll('.tab');
+
+// Function to handle tab click event
+function handleTabClick() {
+  // Remove the 'selected' class from all tabs
+  tabs.forEach(tab => tab.classList.remove('selected'));
+
+  // Add the 'selected' class to the clicked tab
+  this.classList.add('selected');
+}
+
+// Set the 'selected' class to the default tab (Tất cả)
+const defaultTab = document.getElementById('tab1');
+defaultTab.classList.add('selected');
+
+// Attach click event listeners to each tab
+tabs.forEach(tab => tab.addEventListener('click', handleTabClick));
+
+
 function clickCancel(number) {
   document.getElementById("status" + number).innerHTML = "đã hủy";
   document.getElementById("button_cancel" + number).style.display = "none";
@@ -22,3 +45,5 @@ quantityInput.addEventListener("change", function (e) {
     quantityInput.value = 1;
   }
 });
+
+
